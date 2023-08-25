7108f127b2847c6f15250d16fee6eb696225c0a6
dingbum73 <dingbum73@gmail.com>
2023-08-25 20:44:05 +0800

    feat: add middleware\auth.js

---
+++ controllers/lesson-controller.js
@@ -0,0 +1,7 @@
+const lessonController = {
+  getLessons: (req, res) => {
+    res.render('index')
+  }
+}
+
+module.exports = lessonController
--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,20 @@
 const express = require('express')
 const router = express.Router()
 const passport = require('../config/passport')
-const userController = require('../controllers/user')
+const userController = require('../controllers/user-controller')
+const lessonController = require('../controllers/lesson-controller')
 const { generalErrorHandler } = require('../middleware/error-handler')
+const { authenticated } = require('../middleware/auth')
 
 router.get('/signin', userController.signInPage)
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
-router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
 router.get('/logout', userController.logout)
 
-router.get('/', (req, res) => res.render('index'))
+router.get('/', authenticated, lessonController.getLessons)
+
+router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
+router.get('/', (req, res) => res.redirect('index'))
 router.use('/', generalErrorHandler)
 
 module.exports = router

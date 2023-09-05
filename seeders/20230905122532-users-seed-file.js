07e02db438cddb88f5f767a999895b10925ba1f2
dingbum73 <dingbum73@gmail.com>
2023-09-05 21:24:04 +0800

    feat: users seed

---
+++ seeders/20230905122532-users-seed-file.js
@@ -0,0 +1,45 @@
+'use strict'
+const bcrypt = require('bcryptjs')
+const faker = require('faker')
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('Admins', [{
+      name: 'root',
+      email: 'root@example.com',
+      password: await bcrypt.hash('12345678', 10),
+      created_at: new Date(),
+      updated_at: new Date()
+    }], {})
+    await queryInterface.bulkInsert('Users', [{
+      name: 'user1',
+      email: 'user1@example.com',
+      password: await bcrypt.hash('12345678', 10),
+      image: `https://loremflickr.com/150/150/happy,human/?random=${Math.random() * 100}`,
+      introduction: faker.lorem.text(),
+      nation: faker.address.country(),
+      created_at: new Date(),
+      updated_at: new Date()
+    }], {})
+    let users = Array.from({ length: 50 }).map(async () => {
+      const hashedPassword = await bcrypt.hash('12345678', 10)
+      return {
+        name: faker.name.findName(),
+        email: faker.internet.exampleEmail(),
+        password: hashedPassword,
+        image: `https://loremflickr.com/150/150/happy,human/?random=${Math.random() * 100}`,
+        introduction: faker.lorem.text(),
+        nation: faker.address.country(),
+        created_at: new Date(),
+        updated_at: new Date()
+      }
+    })
+    users = await Promise.all(users)
+    await queryInterface.bulkInsert('Users', users)
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Admins', {})
+    await queryInterface.bulkDelete('Users', {})
+  }
+}
